055a4cce3c8a90ab0c27fc2806786154d71a59f4
Rodrigo Linniker <rodrigo_linnik123@hotmail.com>
2025-09-22 14:29:24 -0300

    Update _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Toast from "react-native-toast-message";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../styles/global.css";
-import RootLayout from ".";
+import RootLayout from "./_index";
 import { FavoritesProvider } from "../context/FavoritesContext";
 
 const queryClient = new QueryClient();
